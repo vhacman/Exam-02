db725ea0d3a7c08758da0b5650da80ac7f186b67
Gabri <hacmanvioricagabriela@gmail.com>
2025-04-27 15:25:47 +0200

    correzioni

--- lev_1/union.c
+++ lev_1/union.c
@@ -12,55 +12,65 @@
 
 #include <unistd.h>
 
-/* Verifica se il carattere 'c' è già apparso nella stringa 'str' fino alla posizione 'pos' */
-int is_printed(char c, char *str, int pos)
+/* Verifica se il carattere 'c' è già apparso nella
+stringa 'str' fino alla posizione 'current' */
+int	is_printed(char c, char *str, int current)
 {
-    int i = 0;
-
-    while (i < pos)
-    {
-        if (str[i] == c)
-            return (1);
-        i++;
-    }
-    return (0);
+	int i = 0;
+	while (i < current)
+	{
+		if (str[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
 }
 
 /* Controlla se 'c' è presente in tutta la stringa 'str' */
-int is_in_str(char c, char *str)
+int	is_in_str(char c, char *str)
 {
-    int i = 0;
-
-    while (str[i])
-    {
-        if (str[i] == c)
-            return (1);
-        i++;
-    }
-    return (0); 
+	int i = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+			return (1);
+		i++;
+	}
+	return (0); 
 }
 
-int main(int ac, char **av)
+/* Stampa i caratteri unici della prima stringa */
+void	print_first(char *first)
 {
-    char    *first = av[1];
-    char    *second = av[2];
-    int     i = 0;
-    if(ac == 3)
-    {
-        while (first[i])
-        {
-            if (!is_printed(first[i], first, i))
-                write(1, &first[i], 1);
-            i++;
-        }
-        i = 0;
-        while (second[i])
-        {
-            if (!is_in_str(second[i], first) && !is_printed(second[i], second, i))
-                write(1, &second[i], 1);
-            i++;
-        }
-        write(1, "\n", 1);
-        return (0);
-    }
+	int i = 0;
+	while (first[i])
+	{
+		if (!is_printed(first[i], first, i))
+			write(1, &first[i], 1);
+		i++;
+	}
+}
+
+/* Stampa i caratteri unici della seconda stringa che non sono nella prima */
+void	print_second(char *second, char *first)
+{
+	int i = 0;
+	while (second[i])
+	{
+		if (!is_in_str(second[i], first) && !is_printed(second[i], second, i))
+			write(1, &second[i], 1);
+		i++;
+	}
+}
+
+/* Main principale */
+int	main(int ac, char **av)
+{
+	if (ac == 3)
+	{
+		print_first(av[1]);
+		print_second(av[2], av[1]);
+	}
+	write(1, "\n", 1);
+	return (0);
 }
