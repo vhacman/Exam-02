504a54450401de4068d080c8f7807409988d9c79
Gabri <hacmanvioricagabriela@gmail.com>
2025-04-30 22:51:44 +0200

    ok

--- lev_3/ft_split.c
+++ lev_3/ft_split.c
@@ -9,77 +9,79 @@
 /*   Updated: 2025/04/26 11:52:35 by vhacman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+#include <unistd.h>
 #include <stdlib.h>
 
-int	is_delimiter(char c)
+int is_delimiter(char c)
 {
-	return (c == ' ' || c == '\t' || c == '\n');
+	return(c == ' ' || c == '\t' || c == '\n');
 }
 
-int	ft_wordlen(char *str)
+int ft_wordlen(char *str)
 {
-	int	i = 0;
-
-	while(str[i] && !is_delimiter(str[i]))
+	int i = 0;
+	while(str[i] && str[i] != '\t' && str[i] != '\n' && str[i] != ' ')
 		i++;
 	return(i);
 }
 
-char	*word_dup(char *str)
-{
-	int		i = 0;
-	int		len = ft_wordlen(str);
-	char	*word = malloc(len + 1);
-
-	if (!word)
-		return(NULL);
-	while (i < len)
-	{
-		word[i] = str[i];
-		i++;
-	}
-	word[i] = '\0';
-	return (word);
-}
-
-int	count_words(char *str)
+int ft_wordcount(char *str)
 {
 	int count = 0;
+	int i = 0;
 
-	while (*str)
+	while(str[i])
 	{
-		while(is_delimiter(*str))
-			str++;
-		if (*str)
+		while(is_delimiter(str[i]))
+			i++;
+		if(str[i] && !is_delimiter(str[i]))
 		{
 			count++;
-			while(*str && !is_delimiter(*str))
-				str++;	
+			while(str[i] && !is_delimiter(str[i]))
+				i++;
 		}
 	}
-	return (count);
+	return(count);
 }
 
-char	**ft_split(char *str)
+char	*word_dup(char *str)
+{
+	int i = 0;
+	int word_len = ft_wordlen(str);
+	char *new;
+
+	new = malloc(sizeof(char) * (word_len + 1));
+	if(!new)
+		return(NULL);
+	while(str[i] && i < word_len)
+	{
+		new[i] = str[i];
+		i++;
+	}
+	new[i] = '\0';
+	return(new);
+}
+
+char **ft_split(char *str)
 {
 	int		i = 0;
-	int		words = count_words(str);
-	char	**array = malloc(sizeof(char *) * (words + 1));
+	int		j = 0;
+	int		counted = ft_wordcount(str);
+	char	**array = malloc(sizeof(char *) * (counted + 1));
 
-	if (!array)
-		return (NULL);
-	while (*str)
+	if(!array)
+		return(NULL);
+	while(str[i])
 	{
-		while (is_delimiter(*str))
-			str++;
-		if (*str)
+		while(is_delimiter(str[i]))
+			i++;
+		if(str[i] && !is_delimiter(str[i]))
 		{
-			array[i++] = word_dup(str);
-			while (*str && !is_delimiter(*str))
-				str++;
+			int	word_len = ft_wordlen(&str[i]);
+			array[j++] = word_dup(&str[i]);
+			i = word_len + i;
 		}
 	}
-	array[i] = NULL;
-	return (array);
+	array[j] = NULL;
+	return(array);
 }
