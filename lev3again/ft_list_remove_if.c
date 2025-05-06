90f7dcf13c4f609399581c508c29aeb54cb95cd9
Gabri <hacmanvioricagabriela@gmail.com>
2025-05-06 19:57:30 +0200

    help

--- lev3again/ft_list_remove_if.c
+++ lev3again/ft_list_remove_if.c
@@ -1,10 +1,5 @@
-#include <unistd.h>
-
-typedef struct      s_list
-{
-    struct s_list   *next;
-    void            *data;
-}                   t_list;
+#include <stdlib.h>
+#include "ft_list.h"
 
 void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 {
--- lev3againagain/ft_list_remove_if.c
+++ lev3againagain/ft_list_remove_if.c
@@ -1,22 +1,21 @@
-#include <stdlib.h>
-#include "ft_list.h"
+#include <stdio.h>
+#include "list.h"
 
-void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
+void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 {
-	t_list	*tmp;
+    t_list *temp;
+    t_list  *current;
 
-	if ((*begin_list == NULL) || (begin_list == NULL))
-		return ;
-	
-	t_list *current = *begin_list;
-
-	if (cmp((*begin_list)->data, data_ref) == 0x0)
-	{
-		tmp = *begin_list;
-		*begin_list = current->next;
-		free(tmp);
-		ft_list_remove_if(begin_list, data_ref, cmp);
-	}
-	else
-		ft_list_remove_if(&current->next, data_ref, cmp);
+    if(begin_list == NULL || *begin_list == NULL)
+        return;
+    current = *begin_list;
+    if(cmp((*begin_list)->data, data_ref) == 0)
+    {
+        temp = *begin_list;
+        *begin_list = current->next;
+        free(temp);
+        ft_list_remove_if(begin_list, data_ref, cmp);
+    }
+    else
+        ft_list_remove_if(&current->next, data_ref, cmp);
 }
