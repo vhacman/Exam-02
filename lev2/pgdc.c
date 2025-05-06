0753e353a0f01a477b3fc277d5c04fac077925ea
Gabri <hacmanvioricagabriela@gmail.com>
2025-05-06 19:46:21 +0200

    again

--- lev2/pgdc.c
+++ lev2/pgdc.c
@@ -1,11 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
 
 unsigned int pgdc(unsigned int a, unsigned int b)
 {
+    int temp;
     while(b != 0)
     {
-        unsigned int temp = b;
+        temp = b;
         b = a % b;
         a = temp;
     }
-    return a;
+    return (a);
+}
+
+int main(int ac, char **av)
+{
+    if(ac == 3)
+    {
+        int n1 = atoi(av[1]);
+        int n2 = atoi(av[2]);
+        if( n1 > 0 && n2 > 0)
+            printf("%d", pgdc(n1, n2));
+    }
+    printf("\n");
+    return(0);
 }
