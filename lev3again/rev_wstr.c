4904e82b7f538f26ef20bf74d557d8a2e22b1d4e
Gabri <hacmanvioricagabriela@gmail.com>
2025-05-07 10:03:22 +0200

    ok

--- lev3again/rev_wstr.c
+++ lev3again/rev_wstr.c
@@ -1,53 +1,41 @@
 #include <unistd.h>
 
-int is_delimiter(char c)
-{
-    return(c == ' ' || c == '\n' || c == '\n');
-}
-
-int rskip_space(char *str, int i)
-{
-    while(str[i] && is_delimiter)
-        i--;
-    return i;
-}
-
+/* Returns the length of the string */
 int ft_strlen(char *str)
 {
     int i = 0;
-    while(str[i])
+    while (str[i])
         i++;
     return i;
 }
 
+/* Moves backward until the previous space or start of string */
 int rskip_word(char *str, int i)
 {
-    while(str[i] && !is_delimiter(str[i]))
+    while (i >= 0 && str[i] != ' ')
         i--;
-    return i; 
+    return i;
 }
 
-void    rev_wstr(char *str)
+/* Prints the words of the string in reverse order */
+void rev_wstr(char *str)
 {
     int i = ft_strlen(str) - 1;
     int start;
 
-    while(i >= 0)
+    while (i >= 0)
     {
-        i = rskip_space(str, i);
-        if(i < 0)
-            break;
         start = rskip_word(str, i);
-        write(1, &str[start + 1], 1);
-        i = start;
-        if(i > 0)
+        write(1, &str[start + 1], i - start);
+        i = start - 1;
+        if (i >= 0)
             write(1, " ", 1);
     }
 }
 
 int main(int ac, char **av)
 {
-    if(ac == 2)
+    if (ac == 2)
         rev_wstr(av[1]);
     write(1, "\n", 1);
     return 0;
--- lev3againagain/ft_list_remove_if.c
+++ lev3againagain/ft_list_remove_if.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-#include "list.h"
+
+typedef struct      s_list
+{
+    struct s_list   *next;
+    void            *data;
+}                   t_list;
 
 void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 {
