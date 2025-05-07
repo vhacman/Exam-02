fc71d15513524daf2292872e92661f1c46a16305
Gabri <hacmanvioricagabriela@gmail.com>
2025-05-07 18:23:22 +0200

    ok

--- lev3again/ft_list_remove_if.c
+++ lev3again/ft_list_remove_if.c
@@ -3,20 +3,18 @@
 
 void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 {
-	t_list	*tmp;
+	t_list **curr = begin_list;
+	t_list *tmp;
 
-	if ((*begin_list == NULL) || (begin_list == NULL))
-		return ;
-	
-	t_list *current = *begin_list;
-
-	if (cmp((*begin_list)->data, data_ref) == 0x0)
+	while (*curr)
 	{
-		tmp = *begin_list;
-		*begin_list = current->next;
-		free(tmp);
-		ft_list_remove_if(begin_list, data_ref, cmp);
+		if (cmp((*curr)->data, data_ref) == 0)
+		{
+			tmp = *curr;
+			*curr = (*curr)->next;
+			free(tmp);
+		}
+		else
+			curr = &(*curr)->next;
 	}
-	else
-		ft_list_remove_if(&current->next, data_ref, cmp);
 }
