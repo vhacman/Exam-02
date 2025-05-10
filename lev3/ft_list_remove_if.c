22977412ecd51c90cab2af6c487c9be6c29da727
Gabri <hacmanvioricagabriela@gmail.com>
2025-05-10 21:11:55 +0200

    aiutoooo

--- lev3/ft_list_remove_if.c
+++ lev3/ft_list_remove_if.c
@@ -8,17 +8,18 @@ typedef struct      s_list
 
 void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 {
-	t_list	*tmp;
+	t_list **curr = begin_list;
+	t_list *tmp;
 
-	if (begin_list == NULL || *begin_list == NULL)
-		return ;                                                                                                                                                                             
-	if (cmp((*begin_list)->data, data_ref) == 0)
+	while (*curr)
 	{
-		tmp = *begin_list;
-		*begin_list = (*begin_list)->next;
-		free(tmp);
-		ft_list_remove_if(begin_list, data_ref, cmp);
+		if (cmp((*curr)->data, data_ref) == 0)
+		{
+			tmp = *curr;
+			*curr = (*curr)->next;
+			free(tmp);
+		}
+		else
+			curr = &(*curr)->next;
 	}
-	else
-		ft_list_remove_if(&((*begin_list)->next), data_ref, cmp);
 }
--- lev3againagain/ft_list_remove_if.c
+++ lev3againagain/ft_list_remove_if.c
@@ -1,26 +1,18 @@
-#include <stdio.h>
-
-typedef struct      s_list
+#include "list.h"
+void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 {
-    struct s_list   *next;
-    void            *data;
-}                   t_list;
+	t_list **curr = begin_list;
+	t_list *tmp;
 
-void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
-{
-    t_list *temp;
-    t_list  *current;
-
-    if(begin_list == NULL || *begin_list == NULL)
-        return;
-    current = *begin_list;
-    if(cmp((*begin_list)->data, data_ref) == 0)
-    {
-        temp = *begin_list;
-        *begin_list = current->next;
-        free(temp);
-        ft_list_remove_if(begin_list, data_ref, cmp);
-    }
-    else
-        ft_list_remove_if(&current->next, data_ref, cmp);
+	while (*curr)
+	{
+		if (cmp((*curr)->data, data_ref) == 0)
+		{
+			tmp = *curr;
+			*curr = (*curr)->next;
+			free(tmp);
+		}
+		else
+			curr = &(*curr)->next;
+	}
 }
