1a2ff0273e20f21a8bd605e1bfb5b4ba4d162f0e
Gabri <hacmanvioricagabriela@gmail.com>
2025-05-12 20:56:04 +0200

    AAAH

--- lev3againagain/ft_list_remove_if.c
+++ lev3againagain/ft_list_remove_if.c
@@ -5,7 +5,6 @@ typedef struct      s_list
 }                   t_list;
 #include <stdlib.h>
 
-
 void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 {
 	t_list *tmp;
---
+++ lev3againagain/sort_list.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+
+typedef	struct s_list
+{
+	int				data;
+	struct s_list	*next;
+}	t_list;
+
+t_list	*sort_list(t_list* lst, int (*cmp)(int, int))
+{
+	t_list *start = lst;
+	int swap;
+
+	while(lst && lst->next)
+	{
+		if((*cmp)(lst->data, lst->next->data) == 0)
+		{
+			swap = lst->data;
+			lst->data = lst->next->data;
+			lst->next->data = swap;
+			lst = start;
+		}
+		else
+			lst = lst->next;
+	}
+	return start;
+}
