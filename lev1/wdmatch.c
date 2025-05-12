cf5dc2c05e857e5e954de26400da75292f6b881c
Viorica Gabriela Hacman <vhacman@e4r8p2.42roma.it>
2025-05-12 18:11:40 +0200

    helpmeplease

--- lev1/wdmatch.c
+++ lev1/wdmatch.c
@@ -1,30 +1,21 @@
 #include <unistd.h>
 
-void print_str(char *s)
-{
-	int i = 0;
-	while (s[i])
-		write(1, &s[i++], 1);
-}
-
-int wdmtch(char *s1, char *s2)
-{
-	int i = 0;
-	int j = 0;
-
-	while (s1[i] && s2[j])
-	{
-		if (s1[i] == s2[j])
-			i++;
-		j++;
-	}
-	return (s1[i] == '\0');
-}
-
 int main(int ac, char **av)
 {
-	if (ac == 3 && wdmtch(av[1], av[2]))
-		print_str(av[1]);
-	write(1, "\n", 1);
-	return 0;
+    if (ac == 3)
+    {
+        int i = 0;
+        int j = 0;
+
+        while (av[1][i] && av[2][j])
+        {
+            if (av[1][i] == av[2][j])
+                i++;
+            j++;
+        }
+        if (av[1][i] == '\0')
+            write(1, av[1], i);
+    }
+    write(1, "\n", 1);
+    return 0;
 }
--- lev1_again/inter.c
+++ lev1_again/inter.c
@@ -1,31 +1,32 @@
 #include <unistd.h>
 
-void    inter(char *s1, char *s2)
+void	inter(char *s1, char *s2)
 {
-    int i = 0;
-    int j;
-    char seen[256] = {0};
-    while(s1[i])
-    {
-        j = 0;
-        while(s2[j])
-        {
-            if(s1[i] == s2[j] && !seen[(unsigned char)s1[i]])
-            {
-                write(1, &s1[i], 1);
-                seen[(unsigned char)s1[i]] = 1;
-                break;
-            }
-            j++;
-        }
-        i++;
-    }
+	int i = 0;
+	int j;
+	int seen[256] = {0};
+	
+	while(s1[i])
+	{
+		j = 0;
+		while(s2[j])
+		{
+			if(s1[i] == s2[j] && !seen[(unsigned char)s1[i]])
+			{
+				write(1, &s1[i], 1);
+				seen[(unsigned char)s1[i]] = 1;
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
 }
 
 int main(int ac, char **av)
 {
-    if(ac == 3)
-        inter(av[1], av[2]);
-    write(1, "\n", 1);
-    return 0;
+	if(ac == 3)
+		inter(av[1], av[2]);
+	write(1, "\n", 1);
+	return 0;
 }
