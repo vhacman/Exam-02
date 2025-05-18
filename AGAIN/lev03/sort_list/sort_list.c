1f480db0c6899706408eb57edad8a2029615e90f
Gabri <hacmanvioricagabriela@gmail.com>
2025-05-18 12:36:51 +0200

    corretto

--- AGAIN/lev03/sort_list/sort_list.c
+++ AGAIN/lev03/sort_list/sort_list.c
@@ -5,7 +5,7 @@ t_list	*sort_list(t_list* lst, int (*cmp)(int, int))
 	t_list	*start = lst;
 	int temp;
 
-	while(lst && lst->next->data)
+	while(lst && lst->next)
 	{
 		if((*cmp)(lst->data, lst->next->data) == 0)
 		{
